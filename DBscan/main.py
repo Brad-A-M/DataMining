1b5e381d95269bc64e949c55ef058fefa3739307
Brad McCoy <bm@Bs-MacBook-Air.local>
2020-10-14 14:38:00 -0600

    Clean up

--- DBscan/main.py
+++ DBscan/main.py
@@ -4,50 +4,39 @@ from scipy.spatial.distance import pdist
 from scipy.spatial.distance import squareform
 
 epsilon = 2
-def euclideanDistance(row1, row2):
-    distance = 0
-    for i in range(len(row1)):
-        distance += np.power((row2[i] - row1[i]), (2))
-    distance = np.sqrt(distance)
-    return distance
-
+minPts = 2
 d = {'c1': [1, 0, 1, 10, 11, 10],'c2': [1, 0, 0, 10,  10, 11]}
 
+def init():
+    df = pd.DataFrame(data=d)
+    dex = []
+    for i in range(0,len(df.index)):
+        dex.append(i)
+    return df, dex
 
-df = pd.DataFrame(data=d)
-#print(df)
-#print(df.index)
+def createPairwiseDistanceMatrix(df):
+    m = pd.DataFrame(squareform(pdist(df.loc[dex])),columns=df.index,index =df.index)
+    return m
 
-dex = []
-for i in range(0,len(df.index)):
-    dex.append(i)
-
-#print(dex)
-
-#print(squareform(pdist(df.loc[dex])))
-
-m = pd.DataFrame(squareform(pdist(df.loc[dex])),columns=df.index,index =df.index)
-#print(m)
-
-lessThanEpsilonDict = {} 
-
-
-#print(lessThanEpsilonDict)
-
-for i in range(len(df)):
-    for j in range(len(df)):
-        if m.loc[i,j]< epsilon:
-            if i in lessThanEpsilonDict.keys():
-                oldValueArray = []
-                oldValueArray = lessThanEpsilonDict[i]
-                oldValueArray.append(j)
-                lessThanEpsilonDict[i] = oldValueArray
-            else:
-                arrayToAppend = []
-                arrayToAppend.append(j)
-                lessThanEpsilonDict[i] = arrayToAppend
+def createLessThanEpsilonDict(df, m):
+    lessThanEpsilonDict = {}
+    for i in range(len(df)):
+        for j in range(len(df)):
+            if m.loc[i,j]< epsilon:
+                if i in lessThanEpsilonDict.keys():
+                    oldValueArray = []
+                    oldValueArray = lessThanEpsilonDict[i]
+                    oldValueArray.append(j)
+                    lessThanEpsilonDict[i] = oldValueArray
+                else:
+                    arrayToAppend = []
+                    arrayToAppend.append(j)
+                    lessThanEpsilonDict[i] = arrayToAppend
+    return lessThanEpsilonDict
 
 
-for key in lessThanEpsilonDict:
-    print('key: ' + str(key))
-    print('value: ' + str(lessThanEpsilonDict[key]))
+df, dex = init()
+m = createPairwiseDistanceMatrix(df)
+lessThanEpsilonDict = createLessThanEpsilonDict(df, m)
+
+print(lessThanEpsilonDict)
