8dbc339c48afe3415a6f145ad166dd93ef96bce7
annajeanwatson <ajwatson@udel.edu>
2020-10-14 13:48:53 -0600

    easier to read

--- DBscan/main.py
+++ DBscan/main.py
@@ -15,19 +15,19 @@ d = {'c1': [1, 0, 1, 10, 11, 10],'c2': [1, 0, 0, 10,  10, 11]}
 
 
 df = pd.DataFrame(data=d)
-print(df)
-print(df.index)
+#print(df)
+#print(df.index)
 
 dex = []
 for i in range(0,len(df.index)):
     dex.append(i)
 
-print(dex)
+#print(dex)
 
-print(squareform(pdist(df.loc[dex])))
+#print(squareform(pdist(df.loc[dex])))
 
 m = pd.DataFrame(squareform(pdist(df.loc[dex])),columns=df.index,index =df.index)
-print(m)
+#print(m)
 
 lessThanEpsilonDict = {} 
 
@@ -39,3 +39,8 @@ for i, mRow in m.iterrows():
             lessThanEpsilonDict[str(mRow)] = oldValueArray
         else:
             lessThanEpsilonDict[str(mRow)] = [i]
+
+#print(lessThanEpsilonDict)
+for key in lessThanEpsilonDict:
+    print('key: ' + key)
+    print('value: ' + str(lessThanEpsilonDict[key]))
